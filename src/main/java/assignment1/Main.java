c3f4626a6afe510066ca09e47ea9420ed43b259f
Cozma Rares <cozmarares11@gmail.com>
2023-03-15 14:56:49 +0200

    Implement the constructor for Polynomial, override toString and fix subtract

--- src/main/java/assignment1/Main.java
+++ src/main/java/assignment1/Main.java
@@ -1,7 +1,35 @@
 package assignment1;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+    public static void main(String[] args) {
+        var p1 = new Polynomial("x^2+1");
+        var p2 = new Polynomial("-2x");
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p1.subtract(p2));
+
+        // String[] strs = {
+        // "+2.2x^2",
+        // "-2.2x^2",
+        // "2.2x^2",
+        // "+x^2",
+        // "-x^2",
+        // "x^2",
+        // "+2.2x",
+        // "-2.2x",
+        // "2.2x",
+        // "+x",
+        // "-x",
+        // "x",
+        // };
+
+        // for (String str : strs) {
+        // for (String s : str.split("x\\^?"))
+        // System.out.println(s + ", " + s.length());
+
+        // System.out.println();
+        // }
+
+    }
 }
--- src/main/java/assignment1/Polynomial.java
+++ src/main/java/assignment1/Polynomial.java
@@ -1,10 +1,13 @@
 package assignment1;
 
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Polynomial {
-    private final static String regex = "(\\+|-)?([1-9]\\d*(\\.\\d+)?)?x?(\\^[1-9]\\d*)?";
+    public final static Pattern monomialPattern = Pattern.compile("[-+]?((\\d+(\\.\\d+)?)?x(\\^\\d+)?|\\d+(\\.\\d+)?)");
 
     private Map<Integer, Double> equation;
 
@@ -19,6 +22,47 @@ public class Polynomial {
 
     public Polynomial(String polynomial) {
         this.equation = new HashMap<>();
+
+        Matcher matcher = monomialPattern.matcher(polynomial);
+
+        while (matcher.find()) {
+            String monomial = matcher.group();
+
+            int positionOfX = monomial.indexOf("x");
+
+            int power;
+            double coefficient;
+
+            if (positionOfX == -1) {
+                power = 0;
+                coefficient = Double.parseDouble(monomial) + this.equation.getOrDefault(power, 0d);
+                this.equation.put(power, coefficient);
+                continue;
+            }
+
+            String[] numbers = monomial.split("x\\^?");
+
+            if (numbers.length == 0) {
+                power = 1;
+                coefficient = 1 + this.equation.getOrDefault(power, 0d);
+                this.equation.put(power, coefficient);
+                continue;
+            }
+
+            String coefficientString = numbers[0];
+
+            if (coefficientString.length() == 0)
+                coefficient = 1;
+            else if (coefficientString.length() == 1 && "+-".indexOf(coefficientString.charAt(0)) != -1)
+                coefficient = coefficientString.charAt(0) == '+' ? 1 : -1;
+            else
+                coefficient = Double.parseDouble(coefficientString);
+
+            power = numbers.length == 1 ? 1 : Integer.parseInt(numbers[1]);
+            coefficient += this.equation.getOrDefault(power, 0d);
+
+            this.equation.put(power, coefficient);
+        }
     }
 
     private Polynomial(Map<Integer, Double> equation) {
@@ -26,10 +70,10 @@ public class Polynomial {
     }
 
     public Polynomial add(Polynomial other) {
-        Map<Integer, Double> result = copyHashMap(this.equation);
+        var result = copyHashMap(this.equation);
 
         for (Map.Entry<Integer, Double> entry : other.equation.entrySet()) {
-            double value = entry.getValue() + result.getOrDefault(entry.getKey(), 0d);
+            double value = result.getOrDefault(entry.getKey(), 0d) + entry.getValue();
 
             result.put(entry.getKey(), value);
         }
@@ -38,10 +82,10 @@ public class Polynomial {
     }
 
     public Polynomial subtract(Polynomial other) {
-        Map<Integer, Double> result = copyHashMap(this.equation);
+        var result = copyHashMap(this.equation);
 
         for (Map.Entry<Integer, Double> entry : other.equation.entrySet()) {
-            double value = entry.getValue() - result.getOrDefault(entry.getKey(), 0d);
+            double value = result.getOrDefault(entry.getKey(), 0d) - entry.getValue();
 
             result.put(entry.getKey(), value);
         }
@@ -71,5 +115,36 @@ public class Polynomial {
         Map<Integer, Double> result = new HashMap<>();
 
         return new Polynomial(result);
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat format = new DecimalFormat("0.#");
+
+        String out = "";
+
+        for (var entry : this.equation.entrySet()) {
+            int power = entry.getKey();
+            double coefficient = entry.getValue();
+            String formattedCoefficient = format.format(coefficient);
+
+            if (power == 0) {
+                out += formattedCoefficient;
+                continue;
+            }
+
+            if (coefficient > 0 && out.length() != 0)
+                out += "+";
+
+            if (coefficient != 1)
+                out += formattedCoefficient;
+
+            out += "x";
+
+            if (power != 1)
+                out += "^" + power;
+        }
+
+        return out;
     }
 }
