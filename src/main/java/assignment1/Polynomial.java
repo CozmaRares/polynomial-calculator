639969c52542e5172b54f9370a0d9323c7dbee05
Cozma Rares <cozmarares11@gmail.com>
2023-03-15 13:11:10 +0200

    Add regex for matching monomes and access modifiers for the methods

--- src/main/java/assignment1/Polynomial.java
+++ src/main/java/assignment1/Polynomial.java
@@ -4,47 +4,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Polynomial {
-    Map<Integer, Double> equation;
+    private final static String regex = "(\\+|-)?\\d?x?(\\^\\d)?";
 
-    Polynomial(String polynomial) {
+    private Map<Integer, Double> equation;
+
+    public Polynomial(String polynomial) {
         this.equation = new HashMap<>();
     }
 
-    Polynomial(Map<Integer, Double> equation) {
+    private Polynomial(Map<Integer, Double> equation) {
         this.equation = equation;
     }
 
-    Polynomial add(Polynomial other) {
+    public Polynomial add(Polynomial other) {
         Map<Integer, Double> result = new HashMap<>();
 
         return new Polynomial(result);
     }
 
-    Polynomial subtract(Polynomial other) {
+    public Polynomial subtract(Polynomial other) {
         Map<Integer, Double> result = new HashMap<>();
 
         return new Polynomial(result);
     }
 
-    Polynomial multiply(Polynomial other) {
+    public Polynomial multiply(Polynomial other) {
         Map<Integer, Double> result = new HashMap<>();
 
         return new Polynomial(result);
     }
 
-    Polynomial divide(Polynomial other) {
+    public Polynomial divide(Polynomial other) {
         Map<Integer, Double> result = new HashMap<>();
 
         return new Polynomial(result);
     }
 
-    Polynomial differentiate() {
+    public Polynomial differentiate() {
         Map<Integer, Double> result = new HashMap<>();
 
         return new Polynomial(result);
     }
 
-    Polynomial integrate() {
+    public Polynomial integrate() {
         Map<Integer, Double> result = new HashMap<>();
 
         return new Polynomial(result);
